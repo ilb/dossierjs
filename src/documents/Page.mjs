f2a0224500267531cad84fbf40a2a3281959aa04
ssortia <alexkiyan.lug@gmail.com>
2022-10-21 13:41:48 +0300

    added originalName

--- src/documents/Page.mjs
+++ src/documents/Page.mjs
@@ -5,6 +5,7 @@ export default class Page {
   constructor(file) {
     this.uri = file.path;
     this.name = file.filename;
+    this.originalName = file.originalname;
     this.uuid = file.filename.split('.')[0];
     this.size = file.size;
     this.extension = file.filename.split('.').pop();
